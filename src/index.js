99db4bb70a07e1416e41e2a110d2f7791e26eb1c
Gokul Vks <gokulvks@Gokuls-MacBook-Pro.local>
2025-09-01 15:17:41 +0530

    done..

--- src/index.js
+++ src/index.js
@@ -5,86 +5,70 @@ const path = require('path');
 const server = http.createServer(async (req, res) => {
   if(req.method === 'GET' ){
     if(req.url === '/calculate'){
-      const data = fs.readFileSync('inputs.txt', 'utf-8').split('\n');
-      if(data[2] === 'add'){
-        if(data[0].isNumber === false || data[1].isNumber === false){
-          res.writeHead(400, {'Content-Type': 'text/plain'});
-          res.end('Invalid Number');
+      try{
+        const dat = await fs.readFile('../inputs.txt', 'utf-8');
+        const data = dat.split('\n');
+        if(data[2] === 'add'){
+          if(data[0].isNumber === false || data[1].isNumber === false){
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Invalid Number');
+          }
+          else{
+            const result = Number(data[0]) + Number(data[1]);
+            await fs.writeFile('result.txt', result.toString());
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(result.toString());
+          }
+        }
+        else if(data[2] === 'subtract'){
+          if(data[0].isNumber === false || data[1].isNumber === false){
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Invalid Number');
+          }
+          else{
+            const result = Number(data[0]) - Number(data[1]);
+            await fs.writeFile('result.txt', result.toString());
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(result.toString());
+          }
+        }
+        else if(data[2] === 'multiply'){
+          if(data[0].isNumber === false || data[1].isNumber === false){
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Invalid Number');
+          }
+          else{
+            const result = Number(data[0]) * Number(data[1]);
+            await fs.writeFile('result.txt', result.toString());
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(result.toString());
+          }
+        }
+        else if(data[2] === 'divide'){
+          if(data[0].isNumber === false || data[1].isNumber === false){
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Invalid Number');
+          }
+          else if(Number(data[1]) === 0){
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Division by zero error');
+          }
+          else{
+            const result = Number(data[0]) / Number(data[1]);
+            await fs.writeFile('result.txt', result.toString());
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(result.toString());
+          }
         }
         else{
-          const result = Number(data[0]) + Number(data[1]);
-          try{
-          await fs.writeFile('result.txt', result.toString());
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end(result.toString());
-          }
-          catch(err){
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Unable to write result');
-          }
+          res.writeHead(400, {'Content-Type': 'text/plain'});
+          res.end('Invalid Operator');
         }
       }
-      else if(data[2] === 'subtract'){
-        if(data[0].isNumber === false || data[1].isNumber === false){
-          res.writeHead(400, {'Content-Type': 'text/plain'});
-          res.end('Invalid Number');
-        }
-        else{
-          const result = Number(data[0]) - Number(data[1]);
-          try{
-          await fs.writeFile('result.txt', result.toString());
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end(result.toString());
-          }
-          catch(err){
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Unable to write result');
-          }
-        }
-      }
-      else if(data[2] === 'multiply'){
-        if(data[0].isNumber === false || data[1].isNumber === false){
-          res.writeHead(400, {'Content-Type': 'text/plain'});
-          res.end('Invalid Number');
-        }
-        else{
-          const result = Number(data[0]) * Number(data[1]);
-          try{
-          await fs.writeFile('result.txt', result.toString());
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end(result.toString());
-          }
-          catch(err){
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Unable to write result');
-          }
-        }
-      }
-      else if(data[2] === 'divide'){
-        if(data[0].isNumber === false || data[1].isNumber === false){
-          res.writeHead(400, {'Content-Type': 'text/plain'});
-          res.end('Invalid Number');
-        }
-        else if(Number(data[1]) === 0){
-          res.writeHead(400, {'Content-Type': 'text/plain'});
-          res.end('Division by zero error');
-        }
-        else{
-          const result = Number(data[0]) / Number(data[1]);
-          try{
-          await fs.writeFile('result.txt', result.toString());
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end(result.toString());
-          }
-          catch(err){
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Unable to write result');
-          }
-        }
-      }
-      else{
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.end('Invalid Operator');
+      catch(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        // console.error(err);
+        res.end('Unable to write result');
       }
     };
   } else {
