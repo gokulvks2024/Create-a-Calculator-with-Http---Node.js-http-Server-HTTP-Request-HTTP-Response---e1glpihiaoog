385a769c6888a1bbd44c85d8792e8c0fb56a3f83
Gokul Vks <gokulvks@Gokuls-MacBook-Pro.local>
2025-09-01 16:02:48 +0530

    done....

--- src/index.js
+++ src/index.js
@@ -10,68 +10,79 @@ const server = http.createServer(async (req, res) => {
         const data = dat.split('\n');
         if(data[2] === 'add'){
           if(isNaN(Number(data[0])) || isNaN(Number(data[1]))){
+            res.statusCode = 400;
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end('Invalid Number');
           }
           else{
             const result = Number(data[0]) + Number(data[1]);
             await fs.writeFile('result.txt', result.toString());
+            res.statusCode = 200;
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end(result.toString());
           }
         }
         else if(data[2] === 'subtract'){
           if(isNaN(Number(data[0])) || isNaN(Number(data[1]))){
+            res.statusCode = 400;
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end('Invalid Number');
           }
           else{
             const result = Number(data[0]) - Number(data[1]);
             await fs.writeFile('result.txt', result.toString());
+            res.statusCode = 200;
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end(result.toString());
           }
         }
         else if(data[2] === 'multiply'){
           if(isNaN(Number(data[0])) || isNaN(Number(data[1]))){
+            res.statusCode = 400;
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end('Invalid Number');
           }
           else{
             const result = Number(data[0]) * Number(data[1]);
             await fs.writeFile('result.txt', result.toString());
+            res.statusCode = 200;
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end(result.toString());
           }
         }
         else if(data[2] === 'divide'){
           if(isNaN(Number(data[0])) || isNaN(Number(data[1]))){
+            res.statusCode = 400;
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end('Invalid Number');
           }
           else if(Number(data[1]) === 0){
+            res.statusCode = 400;
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end('Division by zero error');
           }
           else{
             const result = Number(data[0]) / Number(data[1]);
             await fs.writeFile('result.txt', result.toString());
+            res.statusCode = 200;
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end(result.toString());
           }
         }
         else{
           res.writeHead(400, {'Content-Type': 'text/plain'});
+          res.statusCode = 400;
           res.end('Invalid Operator');
         }
       }
       catch(err){
+        res.statusCode = 500;
         res.writeHead(500, {'Content-Type': 'text/plain'});
-        // console.error(err);
         res.end('Unable to write result');
       }
     };
   } else {
+    res.statusCode = 404;
     res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('Not Found');
   }
